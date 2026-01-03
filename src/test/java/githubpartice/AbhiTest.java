c45b0ec1172bb6d97b9a37dbc28551079a46199d
SaiAbhishekB <13saiabhishek@gmail.com>
2026-01-03 13:10:05 +0530

    sdfghjk

---
+++ src/test/java/githubpartice/AbhiTest.java
@@ -0,0 +1,21 @@
+package githubpartice;
+
+import org.testng.annotations.Test;
+
+public class AbhiTest {
+
+	@Test
+	public void methodTest() {
+		System.out.println("this is my class");
+	}
+	
+	@Test
+	public void D_methodTest() {
+		System.out.println("so what...?");
+	}
+	
+	@Test
+	public void A_methodTest() {
+		System.out.println("..........");
+	}
+}
